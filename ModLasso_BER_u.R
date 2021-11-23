a68c973608bb7cc6ea8facd1923ff3200bdecffb
ClarisseTroussard <94287094+ClarisseTroussard@users.noreply.github.com>
2021-11-23 16:18:41 +0100

    Update ModLasso_BER_u.R

--- ModLasso_BER_u.R
+++ ModLasso_BER_u.R
@@ -13,6 +13,7 @@ data = as.data.frame(data)
 
 data_bern = subset(data, station == "BER", select = c(station, datetime, U_10M_mean, V_10M_mean, U_10M, V_10M, u10, v10))
 head(data_bern)
+attach(data_bern)
 
 nrow(data_bern)
 nrow(data)
